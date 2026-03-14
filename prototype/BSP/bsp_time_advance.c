0c114ff0f4c3719402929ad8322fd9e0c06ffbe4
wangwenhua <wangwenhua73@qq.com>
2026-03-14 22:35:23 +0800

    [add]增加高级定时器空白文件

---
+++ prototype/BSP/bsp_time_advance.c
@@ -0,0 +1,6 @@
+#include "bsp_time_advance.h"
+
+
+
+
+
---
+++ prototype/BSP/bsp_time_advance.h
@@ -0,0 +1,20 @@
+#ifndef __BSP_TIM_ADVANCE_H__
+#define __BSP_TIM_ADVANCE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_cortex.h"
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
