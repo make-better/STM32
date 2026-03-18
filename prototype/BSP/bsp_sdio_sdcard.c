ddeb0d4cafd9c5ea9cb32c2fea39d30a96678e83
wangwenhua <wangwenhua73@qq.com>
2026-03-18 22:01:18 +0800

    [add]增加sdcard空白文件

---
+++ prototype/BSP/bsp_sdio_sdcard.c
@@ -0,0 +1,1 @@
+#include "bsp_sdio_sdcard.h"
---
+++ prototype/BSP/bsp_sdio_sdcard.h
@@ -0,0 +1,19 @@
+#ifndef __BSP_SDIO_SDCARD_H__
+#define __BSP_SDIO_SDCARD_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_cortex.h"
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
